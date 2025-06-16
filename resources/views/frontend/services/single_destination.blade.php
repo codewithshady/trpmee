9ef4bb95d02b437b79d162fb5d8c8dae8e1e4b7e
Jayeshworld <githubjd2025@gmail.com>
2025-06-16 18:53:14 +0530

    Auto commit

--- resources/views/frontend/services/single_destination.blade.php
+++ resources/views/frontend/services/single_destination.blade.php
@@ -107,16 +107,19 @@
                                     <span>{{ $destination->language ?? 'English' }}</span>
                                 </li>
                                 <li>
-                                    <p class="destination-details__sidebar__text"> <i class="icon-check-star"></i>Area(Km4):
+                                    <p class="destination-details__sidebar__text"> <i class="icon-check-star"></i>Area:
                                     </p><!-- /.destination-details__sidebar__text -->
-                                    <span>{{ $destination->area ?? '90.000 Km5' }}</span>
+                                    <span>{{ $destination->area ?? '90.000 Km' }}</span>
                                 </li>
                             </ul>
                         </div><!-- /.destination-details__sidebar__item -->
                         <div class="destination-details__sidebar__item destination-details__sidebar__item-map wow fadeInUp"
                             data-wow-duration='1500ms' data-wow-delay='500ms'>
                             @if($destination->map_embed)
-                                {!! $destination->map_embed !!}
+                             <iframe title="template google map"
+                                    src="{!! $destination->map_embed !!}"
+                                    class="map__@@extraClassName" allowfullscreen></iframe>
+                                
                             @else
                                 <iframe title="template google map"
                                     src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4562.753041141002!2d-118.80123790098536!3d34.152323469614075!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80e82469c2162619%3A0xba03efb7998eef6d!2sCostco+Wholesale!5e0!3m2!1sbn!2sbd!4v1562518641290!5m2!1sbn!2sbd"
